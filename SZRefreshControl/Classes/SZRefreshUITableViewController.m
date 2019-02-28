1c7735c9ecd1f357f34b14e23b68497939672bed
Song Zhou <zhousong1993@gmail.com>
2019-02-28 11:06:48 +0800

    up

--- SZRefreshControl/Classes/SZRefreshUITableViewController.m
+++ SZRefreshControl/Classes/SZRefreshUITableViewController.m
@@ -27,25 +27,5 @@
 
 - (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
-    
-    CGFloat width = CGRectGetWidth(self.view.bounds);
-    CGFloat contentHeight = self.tableView.contentSize.height;
-    
-    if (_refreshFooterControl) {
-        self.refreshFooterControl.frame = CGRectMake(0, contentHeight, width, SZ_REFRESH_FOOTER_HEIGHT);
-    }
 }
-
-#pragma mark - Setter
-- (void)setRefreshFooterControl:(SZRefreshFooter *)refreshFooterControl {
-    if (_refreshFooterControl) {
-        [_refreshFooterControl removeFromSuperview];
-    }
-    
-    _refreshFooterControl = refreshFooterControl;
-    _refreshFooterControl.scrollView = self.tableView;
-
-    [self.tableView addSubview:_refreshFooterControl];
-}
-
 @end
