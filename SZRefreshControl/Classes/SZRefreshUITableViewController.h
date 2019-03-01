d7f230016141fbbd3ce3cf812b11152060b404e3
Song Zhou <zhousong1993@gmail.com>
2019-03-01 13:58:31 +0800

    refine

--- SZRefreshControl/Classes/SZRefreshUITableViewController.h
+++ SZRefreshControl/Classes/SZRefreshUITableViewController.h
@@ -10,17 +10,11 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@class SZRefreshFooter;
-@class SZPullToRefreshControl;
 @class SZPullToRefreshController;
 @interface SZRefreshUITableViewController : UITableViewController
 
 @property (nonatomic) SZPullToRefreshController *pullToRefreshController;
 @property (nonatomic) SZPullToRefreshController *footerPullToRefreshController;
-
-@property (nullable, nonatomic) SZRefreshFooter *refreshFooterControl; 
-
-
 
 @end
 
